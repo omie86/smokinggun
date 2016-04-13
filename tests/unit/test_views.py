b2be531422cf2c0c2f83bd00ce28f9e8b2114e89
Joel Colucci <joel.colucci@finalsite.com>
2016-04-13 17:34:52 -0400

    tests: add basic test to see that root path route is serving

---
+++ tests/unit/test_views.py
@@ -0,0 +1,26 @@
+"""Unit tests for Flask application views module.
+"""
+
+
+from unittest import TestCase, main
+
+import mailgunpi
+
+
+class ViewsTestCase(TestCase):
+    def setUp(self):
+        mailgunpi.app.config['TESTING'] = True
+        self.app = mailgunpi.app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_root_view(self):
+        """Test that root '/' returns page"""
+        response = self.app.get('/')
+        
+        assert 'MailgunPI' in response.data
+
+
+if __name__ == '__main__':
+    main()
