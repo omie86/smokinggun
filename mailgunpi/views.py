8b86d59f227b60928096666c6d9f413060e3ab1c
Joel Colucci <joel.colucci@finalsite.com>
2016-04-18 15:46:35 -0400

    feat: return mailgun api if query param domain provided

--- mailgunpi/views.py
+++ mailgunpi/views.py
@@ -5,8 +5,19 @@
 from flask import Flask, render_template, jsonify, make_response, request
 
 from mailgunpi import app
+from mailgunpi.utils import get_events
 
 
 @app.route('/', methods=['GET'])
 def get_root():
-    return render_template('index.html')
+    """Return"""
+    domain = request.args.get('domain')
+    if domain:
+        # Fetch data from Mailgun API
+        response = get_events(domain)
+        json_response = response.json()
+        events = json_response['items']
+    else:
+        events = None
+
+    return render_template('index.html', events=events)
