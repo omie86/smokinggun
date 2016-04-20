a3fb48c217af1844ef450a0ecc04c4a8725b4d70
Joel Colucci <joel.colucci@finalsite.com>
2016-04-20 16:30:31 -0400

    feat: implement very rough usage of json2csv package

--- mailgunpi/views.py
+++ mailgunpi/views.py
@@ -6,6 +6,8 @@ import json
 
 from flask import Flask, redirect, render_template, jsonify, make_response, request, send_from_directory
 
+from json2csv import gen_outline, json2csv
+
 from mailgunpi import app
 from mailgunpi.config import UPLOAD_FOLDER
 from mailgunpi.utils import get_events
@@ -40,15 +42,41 @@ def download():
     json_response = response.json()
     events = json_response['items']
 
+    mailgun_json = {
+        "data": events
+    }
+
     # Write JSON file
-    filename = 'data.csv'
+    filename = 'mailgun_json.json'
+
     json_file_path = './mailgunpi/tempfiles/{}'.format(filename)
+
     with open(json_file_path, 'w') as f:
-        f.write(json.dumps(events))
+        f.write(json.dumps(mailgun_json))
+
+
+    temp_json_file = open(json_file_path, 'r')
+
+    # Generate JSON map file
+    outline = gen_outline.make_outline(json_file=temp_json_file, each_line=False, collection_key='data')
+
+    # Store map file
+    temp_outfile = 'temp.outfile.json'
+    outfile = './mailgunpi/tempfiles/{}'.format(temp_outfile)
+    with open(outfile, 'w') as f:
+        json.dump(outline, f, indent=2, sort_keys=True)
 
     # Convert JSON file to CSV
+    key_map_json = open(outfile)
+    key_map = json.load(key_map_json)
+
+    loader = json2csv.Json2Csv(key_map)
+
+    file_json = open(json_file_path, 'r')
+    loader.load(file_json)
 
     # Write to CSV file
+    loader.write_csv(filename="./mailgunpi/tempfiles/test.csv")
 
     # Return CSV file
-    return send_from_directory(directory=UPLOAD_FOLDER, filename=filename)
+    return send_from_directory(directory=UPLOAD_FOLDER, filename='test.csv')
