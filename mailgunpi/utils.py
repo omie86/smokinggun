d23e3c601521e3e6483225d2c025e7a0d67f4910
Joel Colucci <joel.colucci@finalsite.com>
2016-04-21 12:59:02 -0400

    refactor: set get_events to accept params argument

--- mailgunpi/utils.py
+++ mailgunpi/utils.py
@@ -7,13 +7,13 @@ import requests
 from mailgunpi import app
 
 
-def get_events(domain_name):
+def get_events(domain_name, params=None):
     """Return results from Mailgun API request"""
+    if not params:
+        params = {}
+
     domain = "https://api.mailgun.net/v3/{}/events".format(domain_name)
     return requests.get(
         domain,
         auth=("api", app.config["MAILGUN_API_KEY"]),
-        params={"begin"       : "Sun, 3 Apr 2016 09:00:00 -0000",
-                "ascending"   : "yes",
-                "limit"       :  25,
-                "pretty"      : "yes"})
+        params=params)
