926bd7bdf5f6c726eda263352e0536ec5b47feb8
Joel Colucci <joel.colucci@finalsite.com>
2016-04-22 10:39:38 -0400

    tests: add shell unittest for MailgunClient

---
+++ tests/mailgunclient/test_mailgunclient.py
@@ -0,0 +1,17 @@
+"""Unit tests for the MailgunClient class.
+"""
+
+
+from unittest import TestCase, main
+
+
+class MailgunClientTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    main()
